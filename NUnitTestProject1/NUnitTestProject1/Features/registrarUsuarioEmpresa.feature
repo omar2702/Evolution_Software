4b793c76a464720eed941b4a6ba5242f3f3c2f52
Unknown <unknown@example.com>
2020-10-19 18:25:49 -0500

    Added scenario register user company

---
+++ NUnitTestProject1/NUnitTestProject1/Features/registrarUsuarioEmpresa.feature
@@ -0,0 +1,12 @@
+﻿Feature: registrarUsuarioEmpresa
+
+A short summary of the feature
+
+@tag1
+Scenario: El usuario se encuentra en la pantalla de registro de la pagina web
+	Given como usuario ingreso a la pagina web
+	When como usuario me registro con los datos 
+	| name     | description      | sector | email           | phone  | address             | country | city |
+    | Interlab | Empresa dedicada | 2      | interlabmailcom | 665579 | Monterrico cuadra23 | Peru    | Lima |
+	Then se registra mis datos con exito
+    
---
+++ NUnitTestProject1/NUnitTestProject1/Steps/RegistrarUsuarioEmpresaSteps.cs
@@ -0,0 +1,58 @@
+﻿using InterLab.API.Resources;
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Serialization.Json;
+using System;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace NUnitTestProject1.Features
+{
+    [Binding]
+    public class RegistrarUsuarioEmpresaSteps
+    {
+        public static RestClient restClient;
+        public static RestRequest restRequest;
+        public static IRestResponse response;
+        private static Company new_company;
+
+        [Given(@"como usuario ingreso a la pagina web")]
+        public void GivenComoUsuarioIngresoALaPaginaWeb()
+        {
+            restClient = new RestClient("https://localhost:44367/");
+            restRequest = new RestRequest("api/companies", Method.POST);
+            restRequest.RequestFormat = DataFormat.Json;
+
+        }
+
+        [When(@"como usuario me registro con los datos")]
+        public void WhenComoUsuarioMeRegistroConLosDatos( Table table)
+        {
+            new_company = table.CreateInstance<Company>();
+            var obj = new Company()
+            {
+                Name = new_company.Name,
+                Description = new_company.Description,
+                Sector = new_company.Sector,
+                Email = new_company.Email,
+                Phone = new_company.Phone,
+                Address = new_company.Address,
+                Country = new_company.Country,
+                City = new_company.City
+            };
+            restRequest.AddBody(obj);
+            response = restClient.Execute(restRequest);
+        }
+        
+        [Then(@"se registra mis datos con exito")]
+        public void ThenSeRegistraMisDatosConExito()
+        {
+            var deserialize = new JsonDeserializer();
+            var output = deserialize.Deserialize<CompanyResource>(response);
+            Console.WriteLine(output.Name);
+            Assert.That("Interlab", Is.EqualTo(output.Name));
+
+
+        }
+    }
+}
